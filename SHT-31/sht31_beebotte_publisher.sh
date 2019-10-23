c8d6f2d45716af0e0368c0c5462f804914482c94
ytani01 <yoichi@tanibayashi.jp>
2019-10-24 00:28:26 +0900

    update

--- SHT-31/sht31_beebotte_publisher.sh
+++ SHT-31/sht31_beebotte_publisher.sh
@@ -6,7 +6,7 @@ MYNAME=`basename $0`
 
 TOKEN=token_QDg59iYFLCUqeZLc
 CHNAME=env1
-PUBLISHER=sht31_beebotte_publisher.py
+PUBLISHER="${HOME}/bin/sht31_beebotte_publisher.py"
 ENV_OUT=${HOME}/tmp/env.txt
 
 exec ${PUBLISHER} ${TOKEN} ${CHNAME} -o ${ENV_OUT}
